a3c76083035788bbbb8502bcb3bba8bb74fcbdb3
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-28 21:43:56 +0530

    Commit

--- server/models/address.model.js
+++ server/models/address.model.js
@@ -1,29 +1,30 @@
-import mongoose, { Mongoose } from 'mongoose';
-
+import mongoose from 'mongoose';
 
 const AddressSchema = new mongoose.Schema({
     houseno: {
         type: String,
         required: [true, "House no is required"],
-
     },
     area: {
         type: String,
-        required: [true, "address is required"],
+        required: [true, "Address is required"],
     },
     category: {
         type: String,
-
     },
     coordinates: {
-        lat: Number,
-        lng: Number,
+        lat: {
+            type: Number,
+            required: [true, "Latitude is required"],
+        },
+        lng: {
+            type: Number,
+            required: [true, "Longitude is required"],
+        }
     },
-
 });
 
-
-
+// Create the Address model from the schema
 const Address = mongoose.model('Address', AddressSchema);
 
 export default Address;
