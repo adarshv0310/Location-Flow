7e2bc7803dc0ef94d0f7c6bd8b76665abfca4819
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-28 19:08:56 +0530

    Commit

--- server/index.js
+++ server/index.js
@@ -1,2 +1,44 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import cors from 'cors';
+import {
+    connectDB
+} from './config/db.js';
+
+
+// dotenv config
+dotenv.config();
+
+// mongodb connection
+connectDB();
+
+
+// rest object 
+const app = express();
+
+
+// middlewares
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+
+
+// port 
+
+const port = process.env.PORT || 8080;
+
+
+app.listen(port, () => {
+    console.log(
+        `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+        .bgCyan.white
+    );
+})
