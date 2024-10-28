a58aa3d74834dd89a589eea1fcde96998deecf05
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-28 18:55:15 +0530

    Commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
+    } catch (error) {
+        console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+    }
+};
+
+module.exports = connectDB;
